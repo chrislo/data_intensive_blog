d81d6342b894b6221b8e1176ff82f95632b225ab
Chris Lowis <chris.lowis@gmail.com>
2016-05-26 10:16:21 +0100

    Format the logger to use JSON
    Create a single line of JSON per message. JSON will be easier to parse
    than the default `Logger` format.

--- app.rb
+++ app.rb
@@ -1,7 +1,14 @@
 require 'sinatra'
+require 'json'
 
 def event_log
   @event_log ||= Logger.new(File.join(File.dirname(__FILE__), 'log', 'event.log'))
+
+  @event_log.formatter = proc do |severity, datetime, progname, msg|
+    {time: datetime}.merge(msg).to_json + "\n"
+  end
+
+  @event_log
 end
 
 def log(data)
